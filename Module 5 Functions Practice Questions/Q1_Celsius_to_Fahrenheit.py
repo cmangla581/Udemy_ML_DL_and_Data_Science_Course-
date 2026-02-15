553980ad15754c2e19438c5201e5e5e64eb4326a
Chaitanya Mangla <tafscm@gmail.com>
2026-02-16 00:27:38 +0530

    Add files via upload

---
+++ Module 5 Functions Practice Questions/Q1_Celsius_to_Fahrenheit.py
@@ -0,0 +1,9 @@
+'''
+You are given a temperature in Celsius. Your task is to convert it to Fahrenheit and return the result.
+''' 
+
+def celsius_to_fahrenheit(C): 
+    return (9/5 * C) + 32 
+
+print(celsius_to_fahrenheit(25))
+print(celsius_to_fahrenheit(0)) 
