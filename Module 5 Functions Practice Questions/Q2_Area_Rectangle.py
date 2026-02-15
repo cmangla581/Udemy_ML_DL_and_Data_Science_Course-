106d00885b08bde4091e572bb658fd19101f0a53
Chaitanya Mangla <tafscm@gmail.com>
2026-02-16 00:27:55 +0530

    Add files via upload

---
+++ Module 5 Functions Practice Questions/Q2_Area_Rectangle.py
@@ -0,0 +1,11 @@
+'''
+You are given the length and breadth of a rectangle. Your task is to compute and return the area of the rectangle.
+''' 
+
+def area_of_rectangle(length, breadth): 
+    return length * breadth 
+    
+print(area_of_rectangle(5,3)) 
+print(area_of_rectangle(7.5,2.4)) 
+
+    
