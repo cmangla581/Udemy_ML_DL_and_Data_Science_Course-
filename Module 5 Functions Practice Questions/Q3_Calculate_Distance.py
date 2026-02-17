9cd8a2a756b0201ee3dd37dc39b13a4349e6334d
Chaitanya Mangla <tafscm@gmail.com>
2026-02-18 01:23:06 +0530

    Add files via upload

---
+++ Module 5 Functions Practice Questions/Q3_Calculate_Distance.py
@@ -0,0 +1,9 @@
+'''
+You are given the speed of a vehicle and the time it has traveled. Your task is to compute and return the distance traveled by the vehicle.
+''' 
+
+def calculate_distance(speed, time): 
+    return speed*time 
+    
+print(calculate_distance(60,2))
+print(calculate_distance(50.5,1.5)) 
